0e1db0909d77678f25b7e3bda9c461c6bc9a86ff
robbiec0720 <robbiec0720@gmail.com>
2022-11-04 17:00:03 -0500

    implemented cors

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -2,29 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 8081
 
 const db = require('./queries')
 
-app.use(cors)
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
 )
-app.use('/api', createProxyMiddleware({ 
-    target: 'http://localhost:8081/', //original url
-    changeOrigin: true, 
-    //secure: false,
-    onProxyRes: function (proxyRes, req, res) {
-       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-}));
 
+const corsOptions = { origin : true }
 
-app.get('/', (request, response) => {
+app.get('/', cors(corsOptions), (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
@@ -32,20 +24,20 @@ app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
 
-app.get('/menuItems', db.displayMenu)
-app.get('/menuItems/permanent', db.getMenuItems)
-app.get('/menuItems/:id', db.getItemName)
-app.get('/menuItems/seasonal', db.getSeasonalItems)
-app.post('/menuItems/add', db.addFoodItem)
-app.get('/excess/:start/:end', db.excessReport)
-app.get('/login', db.login)
-app.post('/inventory/add', db.addIngredient)
-app.get('/inventory', db.displayInventory)
-app.get('/inventory/restock-report', db.restockReport)
-app.get('/order/add/:id', db.addItem)
-app.get('/order/remove/:id', db.removeItem)
-app.get('/order/clear', db.clearCart)
-app.post('/order/submit', db.orderSubmitted)
-app.get('/order/display', db.displayOrder)
-app.delete('/entry/delete', db.deleteEntry)
-app.put('/edit', db.editTable)
+app.get('/menuItems', cors(corsOptions), db.displayMenu)
+app.get('/menuItems/permanent', cors(corsOptions), db.getMenuItems)
+app.get('/menuItems/:id', cors(corsOptions), db.getItemName)
+app.get('/menuItems/seasonal', cors(corsOptions), db.getSeasonalItems)
+app.post('/menuItems/add', cors(corsOptions), db.addFoodItem)
+app.get('/excess/:start/:end', cors(corsOptions), db.excessReport)
+app.get('/login', cors(corsOptions), db.login)
+app.post('/inventory/add', cors(corsOptions), db.addIngredient)
+app.get('/inventory', cors(corsOptions), db.displayInventory)
+app.get('/inventory/restock-report', cors(corsOptions), db.restockReport)
+app.get('/order/add/:id', cors(corsOptions), cors(corsOptions), db.addItem)
+app.get('/order/remove/:id', cors(corsOptions), db.removeItem)
+app.get('/order/clear', cors(corsOptions), db.clearCart)
+app.post('/order/submit', cors(corsOptions), db.orderSubmitted)
+app.get('/order/display', cors(corsOptions), db.displayOrder)
+app.delete('/entry/delete', cors(corsOptions), db.deleteEntry)
+app.put('/edit', cors(corsOptions), db.editTable)
