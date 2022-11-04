9adf0b2ba70b38032abd2d0f51422a6575b7ea1a
Uma <uma.e.sarkar@gmail.com>
2022-11-04 10:44:41 -0500

    set up multiple pages and routing stuff pt2

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import Footer from './components/commons/Footer/Footer';
 import Header from './components/commons/Header/Header';
 import EmployeePage from './components/EmployeePage/EmployeePage';
+import Auth from './components/Auth';
+import Manager from './components/EmployeePage/ManagerPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
+    
     <div className="App">
       <Header></Header>
-      <EmployeePage></EmployeePage>
+      <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Auth />} />
+        <Route path="/employee" element ={<EmployeePage />}/>
+        <Route path="/manager" element={<Manager />}/>
+      </Routes>
+    </BrowserRouter>
       <Footer></Footer>
     </div>
   );
---
+++ my-react-app/src/components/EmployeePage/ManagerPage.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function emp(props) {
+  return (
+    <h1>Manager view</h1>
+  )
+}
