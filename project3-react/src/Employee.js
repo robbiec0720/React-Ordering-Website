13e1735dc928989a5fb7c9991157613d064e73ee
Uma <uma.e.sarkar@gmail.com>
2022-11-04 10:56:03 -0500

    staging file deletions

--- project3-react/src/Employee.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function emp(props) {
-  return (
-    <h1>Employee view</h1>
-  )
-}
--- project3-react/src/Manager.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function manager(props) {
-  return (
-    <h1>Manager view</h1>
-  )
-}
