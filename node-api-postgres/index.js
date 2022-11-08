1a4269935422ee0ce9cac829d361449035a54bc4
Cam Quilici <cameron.quilici@gmail.com>
2022-11-08 15:58:32 -0600

    Changed env variable

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 8081
+const port = process.env.PORT || 8081
 
 const db = require('./queries')
 
