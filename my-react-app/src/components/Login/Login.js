3b35c50fea2f7029faf75c1782ace3e0621bfe60
cquil11 <cameron.quilici@gmail.com>
2022-11-21 17:39:48 -0600

    Formatting Login.js

--- my-react-app/src/components/Login/Login.js
+++ my-react-app/src/components/Login/Login.js
@@ -4,32 +4,32 @@ import './Login.css'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const navigate = useNavigate();
-    return (
+  const navigate = useNavigate();
+  return (
     <form className="Auth-form">
-        <div className="Auth-form-content">
-  <h3 className="Auth-form-title">If you're an Employee or a Manager: Sign In</h3>
-  <div className="form-group">
-    <input
-      className="login-input"
-      placeholder="Your name"
-    />
-  </div>
-  <div className="form-group">
-    <input
-      type="password"
-      className="login-input"
-      placeholder="Password"
-    />
-  </div>
-  <div className="">
-    <button type="submit" className="submit-button" onClick={() => navigate('/employee')}>
-      Submit
-    </button>
-  </div>
-</div>
-</form>
-    );
+      <div className="Auth-form-content">
+        <h3 className="Auth-form-title">If you're an Employee or a Manager: Sign In</h3>
+        <div className="form-group">
+          <input
+            className="login-input"
+            placeholder="Your name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+          />
+        </div>
+        <div className="">
+          <button type="submit" className="submit-button" onClick={() => navigate('/employee')}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
