660e4ca137d010121c60e10d0a4c5a69d49e4272
robbiec2024 <robbiec2024@tamu.edu>
2022-11-30 15:49:45 -0600

    test

--- my-react-app/src/components/Auth.js
+++ my-react-app/src/components/Auth.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import "../App.css"
 
+import googleTranslate from "/commons/Translate";
 import { useNavigate } from "react-router-dom";
+import { LangContext } from "../App";
 
 const Auth = () => {
   const navigate = useNavigate();
+  const { lang } = useContext(LangContext)
+
   return (
     <div id="Nav Page">
       <div className="container">
 
         <div id="nav-page-content">
           <div className="nav-left">
-            <h1 class="chickFont">Welcome to Chick-fil-a</h1>
+            <h1 class="chickFont">{googleTranslate.translate('Welcome to Chick-fil-a', 'Dutch')}</h1>
             <h1>Get our Spicy Chicken Sandwich</h1>
             <h3>Send your loved ones Chick-fil-A for the holidays. Introducing order delivery!!!</h3>
             <button className="nav-page-button" onClick={() => navigate('/customer')}>Place an Order</button>
