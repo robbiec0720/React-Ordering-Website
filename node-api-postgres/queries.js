21620bff578eaa99a16091e10000d643d9129214
cquil11 <cameron.quilici@gmail.com>
2022-11-30 11:01:30 -0600

    Added first little bit of documentation in queries.js. WE should complete this sooner than later

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -426,28 +426,35 @@ const login = async (request, response) => {
   }
 }
 
+/**
+ * Function called when an order is submitted. Not to be confused with "placeOrder()"
+ * which actually handles the functionality of placing the current order in the database.
+ * 
+ * @param {*} request API request.
+ * @param {*} response Response in JSON format.
+ */
 const orderSubmitted = async (request, response) => {
-  const id = parseInt(request.query.id)
-  const payType = parseInt(request.query.type)
-  const payment = parseFloat(request.query.payment)
+  const id = parseInt(request.query.id);             
+  const payType = parseInt(request.query.type);
+  const payment = parseFloat(request.query.payment);
 
-  condiments.push(26)
-  condiments.push(27)
+  condiments.push(26);
+  condiments.push(27);
 
-  const orderInfo = await placeOrder(payment, payType)
+  const orderInfo = await placeOrder(payment, payType);
 
   if (orderInfo == null) {
-    response.status(200).json(-1)
+    response.status(200).json(-1);
   }
   else {
-    placeTransaction(orderInfo[0], id, payType, orderInfo[1], payment)
-    updateInventory()
-    response.status(200).json(0)
+    placeTransaction(orderInfo[0], id, payType, orderInfo[1], payment);
+    updateInventory();
+    response.status(200).json(0);
   }
 
   console.log("Succesfully placed order with id = " + id + ", paymentType = " + payment + ", payment = " + payment + ".");
-  order = []
-  condiments = []
+  order = [];
+  condiments = [];
 }
 
 async function placeOrder(payment, payType) {
