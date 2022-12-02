c8dae6a595b8056652fd9d96e9d5a072ecab6ffd
robbiec2024 <robbiec2024@tamu.edu>
2022-12-02 14:17:18 -0600

    translated inventory editing pages

--- my-react-app/src/components/ManageAccessPage/EditInventory.js
+++ my-react-app/src/components/ManageAccessPage/EditInventory.js
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { LangContext, PrevLangContext } from '../../App';
 
 const EditInventory = () => {
+    const { lang } = useContext(LangContext)
+    const { prevLang } = useContext(PrevLangContext)
+    const [report, setReport] = React.useState('Edit Inventory')
+    const [btn, setBtn] = React.useState('Submit')
+    
+    useEffect(() => {
+        let t = [report, btn]
+        let text = t.join(';')
+        if (lang !== prevLang) {
+            const API_KEY = 'AIzaSyANYWkU1YhvNE5flUIvzJv8g-y0KCHva-0'
+            let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`
+            url += '&q=' + encodeURI(text)
+            url += `&source=${prevLang}`
+            url += `&target=${lang}`
+            let translated = new Promise(function (resolve, reject) {
+                fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json"
+                    }
+                })
+                    .then(res => res.json())
+                    .then((response) => {
+                        //console.log("response from google: ", response.data.translations[0].translatedText)
+                        resolve(response.data.translations[0].translatedText)
+                    })
+                    .catch(error => {
+                        console.log("There was an error with the translation request: ", error)
+                    });
+            })
+            translated.then((result) => {
+                var split = result.split(';')
+                console.log(split)
+                setReport(split[0])
+                setBtn(split[1])
+            })
+        }
+    }, [lang, prevLang, btn, report])
+
     // form to edit inventory
     class EditForm extends React.Component {
         constructor(props) {
@@ -62,7 +103,7 @@ const EditInventory = () => {
                         New Value:&nbsp;
                         <input type="text" value={this.state.value} onChange={this.valChange} />
                     </label>
-                    <input className="submit-btn" type="submit" value="Submit" />
+                    <input className="submit-btn" type="submit" value={btn} />
                 </form>
             )
         }
@@ -70,7 +111,7 @@ const EditInventory = () => {
 
     return (
         <div className='form'>
-            <h1>Edit Inventory</h1>
+            <h1>{report}</h1>
             <EditForm></EditForm>
         </div>
     );
