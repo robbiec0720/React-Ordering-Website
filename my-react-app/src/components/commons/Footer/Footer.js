a738d8aff6a84b350add7de6eae697e8cde379e1
cquil11 <cameron.quilici@gmail.com>
2022-12-05 11:27:26 -0600

    Fixed footer issue

--- my-react-app/src/components/commons/Footer/Footer.js
+++ my-react-app/src/components/commons/Footer/Footer.js
@@ -5,11 +5,7 @@ const Footer = () => {
     const {theme}  = useContext(ThemeContext)
     return (
         <footer className={theme === 'light' ? 'footer-style': 'footer-style-dark'}>
-<<<<<<< HEAD
             <h4>Chick-Fil-A</h4>
-=======
-            <h5>Chick-Fil-A</h5>
->>>>>>> umaSprint3
             <p><small>All picture rights reserved by Chick-Fil-A, the rest is Team 1 ©</small></p>
         </footer>
     );
