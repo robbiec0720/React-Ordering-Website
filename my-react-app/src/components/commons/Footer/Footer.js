5dccd9ae6569e53a26bb76b44a0d9a6438339168
robbiec2024 <robbiec2024@tamu.edu>
2022-12-05 18:52:26 -0600

    edited footer

--- my-react-app/src/components/commons/Footer/Footer.js
+++ my-react-app/src/components/commons/Footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     const {theme}  = useContext(ThemeContext)
     return (
         <footer className={theme === 'light' ? 'footer-style': 'footer-style-dark'}>
-            <h4>Chick-Fil-A</h4>
             <p><small>All picture rights reserved by Chick-Fil-A, the rest is Team 1 ©</small></p>
         </footer>
     );
